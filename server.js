1fd267826fe5fcc85ead01396a4ab83d2a85c60a
BrittFromIT <Brittanylewis32@gmail.com>
2018-11-15 10:38:15 -0500

    commit message

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/', (req,res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+  console.log(" AYYEEE WE LIIVVEEE BAYBEHHH " + PORT);
 })
